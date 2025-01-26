cafccd1be25cb3297cbd7f7aea4c61445d6fc8bc
Cong Wang <cong.wang@bytedance.com>
2025-01-26 13:48:48 -0800

    Combine BPF_PROG_LOAD data and original signature for the 2nd phase
    Signed-off-by: Cong Wang <cong.wang@bytedance.com>

--- sign-ebpf.c
+++ sign-ebpf.c
@@ -17,21 +17,14 @@ struct original_data {
     __u32 sig_len;
 };
 
-struct modified_data {
-    __u8 data[MAX_DATA_SIZE];
-};
-
 struct modified_sig {
     __u8 sig[MAX_SIG_SIZE];
     __u32 sig_len;
 };
 
-struct {
-    __uint(type, BPF_MAP_TYPE_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, __u32);
-    __type(value, struct modified_data);
-} data_input SEC(".maps");
+struct combined_buffer {
+    __u8 data[MAX_DATA_SIZE + MAX_SIG_SIZE];
+};
 
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
@@ -47,25 +40,28 @@ struct {
     __type(value, struct original_data);
 } original_program SEC(".maps");
 
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, struct combined_buffer);
+} combined_data_map SEC(".maps");
+
 char _license[] SEC("license") = "GPL";
 
 SEC("lsm.s/bpf")
 int BPF_PROG(bpf, int cmd, union bpf_attr *attr, unsigned int size)
 {
-    struct bpf_dynptr data_ptr, sig_ptr, orig_data_ptr, orig_sig_ptr;
-    struct modified_data *mod_data;
+    struct bpf_dynptr sig_ptr, orig_data_ptr, orig_sig_ptr, combined_data_ptr;
     struct modified_sig *mod_sig;
     struct original_data *orig_data;
     struct bpf_key *trusted_keyring;
+    struct combined_buffer *combined_buf;
     __u32 insn_cnt;
     int ret, zero = 0;
 
     if (cmd != BPF_PROG_LOAD)
         return 0;
-
-    mod_data = bpf_map_lookup_elem(&data_input, &zero);
-    if (!mod_data)
-        return -ENOENT;
 
     mod_sig = bpf_map_lookup_elem(&modified_signature, &zero);
     if (!mod_sig)
@@ -75,22 +71,32 @@ int BPF_PROG(bpf, int cmd, union bpf_attr *attr, unsigned int size)
     if (!orig_data)
         return -ENOENT;
 
-    ret = bpf_probe_read_kernel(&insn_cnt, sizeof(insn_cnt), &attr->insn_cnt);
-    if (ret)
-        goto out;
+    combined_buf = bpf_map_lookup_elem(&combined_data_map, &zero);
+    if (!combined_buf)
+        return -ENOENT;
 
-    ret = bpf_copy_from_user(mod_data, insn_cnt, (void *)(unsigned long)attr->insns);
+    ret = bpf_probe_read_kernel(&insn_cnt, sizeof(insn_cnt), &attr->insn_cnt);
     if (ret)
         goto out;
 
     if (mod_sig->sig_len > sizeof(mod_sig->sig) ||
         orig_data->data_len > sizeof(orig_data->data) ||
-        orig_data->sig_len > sizeof(orig_data->sig))
+        orig_data->sig_len > sizeof(orig_data->sig) ||
+        insn_cnt + orig_data->sig_len > sizeof(combined_buf->data))
         return -EINVAL;
 
     bpf_dynptr_from_mem(orig_data->data, orig_data->data_len, 0, &orig_data_ptr);
     bpf_dynptr_from_mem(orig_data->sig, orig_data->sig_len, 0, &orig_sig_ptr);
-    bpf_dynptr_from_mem(mod_data->data, insn_cnt, 0, &data_ptr);
+
+    // Copy program and original signature into combined buffer
+    ret = bpf_copy_from_user(combined_buf->data, insn_cnt, (void *)(unsigned long)attr->insns);
+    if (ret)
+        goto out;
+    ret = bpf_probe_read(&combined_buf->data[insn_cnt], orig_data->sig_len, orig_data->sig);
+    if (ret)
+        goto out;
+
+    bpf_dynptr_from_mem(combined_buf->data, insn_cnt + orig_data->sig_len, 0, &combined_data_ptr);
     bpf_dynptr_from_mem(mod_sig->sig, mod_sig->sig_len, 0, &sig_ptr);
 
     if (user_keyring_serial)
@@ -107,7 +113,7 @@ int BPF_PROG(bpf, int cmd, union bpf_attr *attr, unsigned int size)
         goto out;
     }
 
-    ret = bpf_verify_pkcs7_signature(&data_ptr, &sig_ptr, trusted_keyring);
+    ret = bpf_verify_pkcs7_signature(&combined_data_ptr, &sig_ptr, trusted_keyring);
     bpf_key_put(trusted_keyring);
 
 out:
